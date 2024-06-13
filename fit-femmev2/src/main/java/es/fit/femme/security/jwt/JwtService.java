24dd1fabe666cf658e5b0b1b0fec48e3447c6961
Melissa Ruiz <125923092+mruizgl@users.noreply.github.com>
2024-06-13 13:19:00 +0100

    Se securizan los metodos. Autentication Required close #46

--- fit-femmev2/src/main/java/es/fit/femme/security/jwt/JwtService.java
+++ fit-femmev2/src/main/java/es/fit/femme/security/jwt/JwtService.java
@@ -1,5 +1,6 @@
 package es.fit.femme.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -11,6 +12,7 @@ import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -40,5 +42,40 @@ public class JwtService {
 
         return Keys.hmacShaKeyFor(keyBytes);
 
+    }
+
+    public String getUsernameFromToken(String token) {
+
+        return getClaims(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username=getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token) {
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token) {
+        return getClaims(token, Claims::getExpiration);
+
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return getExpiration(token).before(new Date());
     }
 }
